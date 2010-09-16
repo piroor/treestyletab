37cbcc89736eee3a647cf2758c6844d8d0e08a06
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2010-09-16 04:07:58 +0000

    Minefieldにおいて、タブバーを自動的に隠す設定のときに、展開された状態のタブバーをリサイズできなくなっていたのを修正
    git-svn-id: http://www.cozmixng.org/repos/piro/treestyletab/trunk@7235 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- modules/utils.js
+++ modules/utils.js
@@ -497,7 +497,7 @@ var TreeStyleTabUtils = {
 	isEventFiredOnClickable : function TSTUtils_isEventFiredOnClickable(aEvent) 
 	{
 		return this.evaluateXPath(
-				'ancestor-or-self::*[contains(" button toolbarbutton scrollbar popup menupopup panel tooltip ", concat(" ", local-name(), " "))]',
+				'ancestor-or-self::*[contains(" button toolbarbutton scrollbar popup menupopup panel tooltip splitter ", concat(" ", local-name(), " "))]',
 				aEvent.originalTarget,
 				Ci.nsIDOMXPathResult.BOOLEAN_TYPE
 			).booleanValue;
