a3d1651ca7a7d5c40795dba08a746386ed6def02
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2011-11-02 02:43:39 +0900

    update mail address

--- modules/window.js
+++ modules/window.js
@@ -17,7 +17,7 @@
  * Portions created by the Initial Developer are Copyright (C) 2011
  * the Initial Developer. All Rights Reserved.
  *
- * Contributor(s): SHIMODA Hiroshi <piro@p.club.ne.jp>
+ * Contributor(s): SHIMODA Hiroshi <piro.outsider.reflex@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
