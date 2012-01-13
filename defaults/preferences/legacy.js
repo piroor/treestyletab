e10bb9b2f397ecbbc1f93b5a465dc5709ea032ce
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2012-01-14 01:17:26 +0900

    delete legacy file for Firefox 3.6

--- defaults/preferences/legacy.js
+++
@@ -1,6 +0,0 @@
-// prefs for leacy features
-pref("extensions.treestyletab.tabbar.transparent.partialTransparency", "0.25");
-
-// for compatibility with old FoxSplitter.
-pref("extensions.treestyletab.tabbar.position.subbrowser.enabled", false);
-
