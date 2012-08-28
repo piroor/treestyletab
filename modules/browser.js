48214c3acfe5b3f56f64760fc0ae737d7eec2901
Infocatcher <infocatcher19@gmail.com>
2012-08-28 14:54:18 +0400

    Add handling of *.default preferences for tab bar size

--- modules/browser.js
+++ modules/browser.js
@@ -2750,9 +2750,9 @@ TreeStyleTabBrowser.prototype = {
 				this.getTreePref('insertNewChildAt') == this.kINSERT_FISRT &&
 				(this.multipleCount <= 0 || this._addedCountInThisLoop <= 0)
 				) {
-				/* �����̎q�^�u����C�ɊJ���ꍇ�A�ŏ��ɊJ�����^�u������
-				   �q�^�u�̍ŏ��̈ʒu�ɑ}�����A�����^�u�́u�ŏ��̊J�����^�u�v��
-				   �u���X�ŏ��̎q�������^�u�v�Ƃ̊Ԃɑ}�����Ă��� */
+				/* 複数の子タブを一気に開く場合、最初に開いたタブだけを
+				   子タブの最初の位置に挿入し、続くタブは「最初の開いたタブ」と
+				   「元々最初の子だったタブ」との間に挿入していく */
 				newIndex = parent._tPos + 1;
 				if (refTab = this.getFirstChildTab(parent))
 					this.insertBefore = refTab.getAttribute(this.kID);
@@ -4679,11 +4679,11 @@ TreeStyleTabBrowser.prototype = {
 	resetTabbarSize : function TSTBrowser_resetTabbarSize() 
 	{
 		if (this.isVertical) {
-			this.clearTreePref('tabbar.shrunkenWidth');
-			this.clearTreePref('tabbar.width');
+			this.setTreePref('tabbar.shrunkenWidth', this.getTreePref('tabbar.shrunkenWidth.default'));
+			this.setTreePref('tabbar.width', this.getTreePref('tabbar.width.default'));
 		}
 		else {
-			this.clearTreePref('tabbar.height');
+			this.setTreePref('tabbar.height', this.getTreePref('tabbar.height.default'));
 			let tabContainerBox = this.getTabContainerBox(this.mTabBrowser);
 			tabContainerBox.removeAttribute('height');
 			this._tabStripPlaceHolder.height = tabContainerBox.boxObject.height;
