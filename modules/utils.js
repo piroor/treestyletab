af929f68584a5f0c9e46fbad03887b73a233349b
saneyuki_s <saneyuki.s.snyk@gmail.com>
2013-01-02 05:57:37 +0900

    Change TreeStyleTabUtils.prefs to simple getter.

--- modules/utils.js
+++ modules/utils.js
@@ -57,8 +57,7 @@ const TST_PREF_PREFIX = 'extensions.treestyletab.';
 let TreeStyleTabUtils = {
 
 	get prefs () {
-		delete this.prefs;
-		return this.prefs = prefs;
+		return prefs;
 	},
 
 /* Save/Load Prefs */
