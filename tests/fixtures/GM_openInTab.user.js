26aa32b622aba4970ee1ebd3b5f2b4efc2f45ab1
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2013-07-03 00:10:21 +0900

    Update sample script for Greasemonkey to test multiple links

--- tests/fixtures/GM_openInTab.user.js
+++ tests/fixtures/GM_openInTab.user.js
@@ -1,9 +1,43 @@
 // ==UserScript==
-// @name			Google First Result in New Tab
-// @namespace		google_firstresult.user.js
-// @description		Open a new tab for the first search result of Google.
-// @include			http*://www.google.*/search?q=*
-//
+// @name           test
+// @namespace      google_GM_openInTab.user.js
+// @include        http*://www.google.*/search?q=*
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @run-at         document-idle
+// see: https://github.com/piroor/treestyletab/issues/517
 // ==/UserScript==
 
-GM_openInTab(document.querySelector('h3 a').href);
+function init()
+{
+    $("div#res.med").prepend('<div><img id="use_GM_openInTab" src="http://i.imgur.com/cQkJVZY.png" title="Open All Links">Use GM_openInTab<br/><br/><br/></div>');
+    $('#use_GM_openInTab').click(function()
+    {
+        useGMopenIn();
+    });
+
+    $("div#res.med").prepend('<div><img id="use_windowOpen" src="http://i.imgur.com/cQkJVZY.png" title="Open All Links">Use window.open<br/></div>');
+    $('#use_windowOpen').click(function()
+    {
+        useWinOpen();
+    });
+}
+
+function useGMopenIn()
+{
+    $('.r a').each(function (i, e) 
+    {       
+        var linkURI = $(e).attr('href');
+        GM_openInTab(linkURI, true);
+    }); return false;
+}
+
+function useWinOpen()
+{
+    $('.r a').each(function (i, e) 
+    {       
+        var linkURI = $(e).attr('href');
+        window.open(linkURI, "_blank");
+    }); return false;
+}
+
+init();
