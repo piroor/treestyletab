10c0108f408336e994ac2f5d84055b8735269f67
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2015-03-01 23:41:39 +0900

    Don't use obsolete String#quote().
    This fixes the issue #821.

--- modules/pseudoTreeBuilder.js
+++ modules/pseudoTreeBuilder.js
@@ -14,7 +14,7 @@
  * The Original Code is the Tree Style Tab.
  *
  * The Initial Developer of the Original Code is YUKI "Piro" Hiroshi.
- * Portions created by the Initial Developer are Copyright (C) 2011-2014
+ * Portions created by the Initial Developer are Copyright (C) 2011-2015
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s): YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
@@ -85,7 +85,7 @@ var PseudoTreeBuilder = {
 			'      }\n' +
 			'    });\n' +
 			'  event.target.dispatchEvent(customEvent);\n' +
-			'}').replace('%EVENT_TYPE%', this.kTAB_LINK_CLICK.quote()));
+			'}').replace('%EVENT_TYPE%', JSON.stringify(this.kTAB_LINK_CLICK)));
 
 		return tree;
 	},
