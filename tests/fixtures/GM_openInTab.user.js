95100cf52cc9b325ca009b78b39c7d95f3d72cc0
YUKI Hiroshi <shimoda@clear-code.com>
2015-10-26 16:40:51 +0900

    Make matching rule more lazy

--- tests/fixtures/GM_openInTab.user.js
+++ tests/fixtures/GM_openInTab.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Open Google's search results in tabs
 // @namespace      google_GM_openInTab.user.js
-// @include        http*://www.google.*/search?q=*
+// @include        http*://www.google.*/search*
 // @grant          GM_openInTab
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @run-at         document-idle
