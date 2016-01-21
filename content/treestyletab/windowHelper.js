de50d19993b598e18161ec3ff458fc8ee0e0479a
YUKI Hiroshi <shimoda@clear-code.com>
2016-01-21 20:57:55 +0900

    Output error

--- content/treestyletab/windowHelper.js
+++ content/treestyletab/windowHelper.js
@@ -80,8 +80,10 @@ var TreeStyleTabWindowHelper = {
 				result = window.__treestyletab__openLinkIn.apply(this, [aUrl, aWhere, aParams].concat(aArgs));
 			}
 			catch(e) {
+				dump(e+'\n');
 			}
-			delete window.__treestyletab__openLinkIn_extraParams;
+			if (window.__treestyletab__openLinkIn_extraParams)
+				delete window.__treestyletab__openLinkIn_extraParams;
 			return result;
 		};
 
@@ -96,8 +98,10 @@ var TreeStyleTabWindowHelper = {
 				result = window.__treestyletab__handleLinkClick.apply(this, [aEvent, aHref, aLinkNode].concat(aArgs));
 			}
 			catch(e) {
+				dump(e+'\n');
 			}
-			delete window.__treestyletab__openLinkIn_extraParams;
+			if (window.__treestyletab__openLinkIn_extraParams)
+				delete window.__treestyletab__openLinkIn_extraParams;
 			return result;
 		};
 
