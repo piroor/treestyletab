f0f62b89ae68f55028d172a5ff8468a388e0c59d
YUKI Hiroshi <shimoda@clear-code.com>
2016-02-17 10:09:23 +0900

    Add credit

--- modules/window.js
+++ modules/window.js
@@ -19,6 +19,7 @@
  *
  * Contributor(s): YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
  *                 Tetsuharu OHZEKI <https://github.com/saneyuki>
+ *                 J. Ryan Stinnett <https://github.com/jryans>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
