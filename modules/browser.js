fbbc1cd11d5e454562c0cf65e88b60cb0e1c77fb
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2016-03-11 02:59:23 +0900

    Prevent to group tabs opened via "loadTabs()", only when it is opened as home page tabs at the startup.
    This fixes a regression #1086 . Now open bookmark group as a tree correctly.

--- modules/browser.js
+++ modules/browser.js
@@ -107,6 +107,7 @@ function TreeStyleTabBrowser(aWindowService, aTabBrowser)
 	this.subTreeChildrenMovingCount = 0;
 	this._treeViewEnabled = true;
 
+	if (prefs.getPref('browser.startup.page') === 1) // prevent to group home page tabs
 	this.nextOpenedTabToBeParent = false;
 }
  
