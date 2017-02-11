b7b7aa79811fea9c1b3133a3190cafd29468efcd
oinkin <oinkin@users.noreply.github.com>
2017-02-11 14:56:32 -0700

    Simple make script

--- make.sh
+++ make.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-cp buildscript/makexpi.sh ./
-./makexpi.sh -n treestyletab -o
-rm ./makexpi.sh
+if [ -z "$SZ_EXE" ]; then
+  SZ_EXE=/cygdrive/C/Program\ Files/7-Zip/7z.exe
+fi
 
+rm -rf build/
+mkdir build/
+cp -r components/ build/
+cp -r content/ build/
+cp -r defaults/ build/
+cp -r locale/ build/
+cp -r modules/ build/
+cp -r skin build/
+cp chrome.manifest build/
+cp icon.png build/
+cp install.rdf build/
+cd build/
+"$SZ_EXE" a -tzip treestyletabforpm@oinkoink.xpi *
