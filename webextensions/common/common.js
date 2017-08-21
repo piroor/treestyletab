3e87a5646cc27d4d589ce69f4a73d6459a03bfe4
YUKI Hiroshi <shimoda@clear-code.com>
2017-08-21 15:31:31 +0900

    Start to develop WebExtensions version

---
+++ webextensions/common/common.js
@@ -0,0 +1,19 @@
+/*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+var configs;
+
+function log(aMessage, ...aArgs)
+{
+	if (!configs || !configs.debug)
+		return;
+
+	console.log('treestyletab: ' + aMessage, ...aArgs);
+}
+
+configs = new Configs({
+	debug : false
+});
