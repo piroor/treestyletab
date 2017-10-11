b7bd6da6636a83862cfa6ace562cd9cb99a4eb78
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-10-11 14:36:12 +0900

    Enable ESLint's indent rule

--- webextensions/.eslintrc.js
+++ webextensions/.eslintrc.js
@@ -14,9 +14,21 @@ module.exports = {
 
   'rules': {
     // stylisitc problem
+    'indent': ['warn', 2, {
+      'SwitchCase': 1,
+      'MemberExpression': 1,
+      'CallExpression': {
+        'arguments': 'first',
+      },
+      'VariableDeclarator': {
+        'var': 2,
+        'let': 2,
+        'const': 3,
+      } 
+    }],
     'quotes': ['warn', 'single', {
       'avoidEscape': true,
       'allowTemplateLiterals': true,
     }],
-  }
+}
 };
