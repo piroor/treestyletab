d9c274d3a1adccdd67adf9cf96618a570f9c17c4
YUKI Hiroshi <shimoda@clear-code.com>
2017-10-11 17:46:24 +0900

    Don't embed too long expression in template

--- webextensions/background/migration.js
+++ webextensions/background/migration.js
@@ -274,12 +274,11 @@ async function notifyUpdatedFromLegacy() {
     active: true
   });
   var title = `${browser.i18n.getMessage('extensionName')} ${browser.runtime.getManifest().version}`
+  var description = browser.i18n.getMessage('message.updatedFromLegacy.description');
   browser.tabs.executeScript(tab.id, {
     code: `
       document.querySelector('#title').textContent = document.title = ${JSON.stringify(title)};
-      document.querySelector('#description').textContent = ${
-        JSON.stringify(browser.i18n.getMessage('message.updatedFromLegacy.description'))
-      };
+      document.querySelector('#description').textContent = ${JSON.stringify(description)};
       location.replace('data:text/html,' + encodeURIComponent(document.documentElement.innerHTML));
     `
   });
