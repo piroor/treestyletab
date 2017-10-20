b21212f7881655fac67d01107739b2296d6cac81
YUKI Hiroshi <shimoda@clear-code.com>
2017-10-20 13:08:41 +0900

    Output more log for debugging

--- webextensions/common/tree/handlers.js
+++ webextensions/common/tree/handlers.js
@@ -165,8 +165,10 @@ function onApiTabUpdated(aTabId, aChangeInfo, aTab) {
   for (let key of Object.keys(aChangeInfo)) {
     updatedTab.apiTab[key] = aChangeInfo[key];
   }
-  if (aTab.openerTabId != updatedTab.apiTab.TSTUpdatedOpenerTabId)
+  if (aTab.openerTabId != updatedTab.apiTab.TSTUpdatedOpenerTabId) {
+    log(`openerTabId of ${aTabId} is changed by someone!: ${updatedTab.apiTab.TSTUpdatedOpenerTabId} => ${aTab.openerTabId}`);
     updatedTab.apiTab.TSTUpdatedOpenerTabId = updatedTab.apiTab.openerTabId = aTab.openerTabId;
+  }
 
   updateTab(updatedTab, aChangeInfo);
   updateParentTab(getParentTab(updatedTab));
