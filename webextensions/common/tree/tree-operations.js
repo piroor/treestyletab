b84aa297c1268e7495116800e52b4525b5b79edb
YUKI Hiroshi <shimoda@clear-code.com>
2017-10-24 15:15:17 +0900

    Fix indent

--- webextensions/common/tree/tree-operations.js
+++ webextensions/common/tree/tree-operations.js
@@ -931,9 +931,9 @@ async function moveTabs(aTabs, aOptions = {}) {
           gAllTabs.appendChild(container);
         }
         if (isAcrossWindows) {
-        container.toBeOpenedTabsWithPositions += aTabs.length;
-        container.toBeOpenedOrphanTabs        += aTabs.length;
-        container.toBeAttachedTabs            += aTabs.length;
+          container.toBeOpenedTabsWithPositions += aTabs.length;
+          container.toBeOpenedOrphanTabs        += aTabs.length;
+          container.toBeAttachedTabs            += aTabs.length;
         }
       };
       if (newWindow) {
