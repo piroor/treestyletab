040c0c15c6eb307adbb2fedc5b0acfcadd176f90
YUKI Hiroshi <shimoda@clear-code.com>
2017-11-07 21:11:24 +0900

    Don't move back an initial restored tab in a restored window

--- webextensions/background/handlers.js
+++ webextensions/background/handlers.js
@@ -275,9 +275,14 @@ function onTabMoving(aTab, aMoveInfo) {
   if (container.openingCount > 0 &&
       !aMoveInfo.byInternalOperation &&
       positionControlled) {
+    let opener = getTabById(aTab.apiTab.openerTabId);
+    // if there is no valid opener, it can be a restored initial tab in a restored window
+    // and can be just moved as a part of window restoration process.
+    if (opener && opener != aTab) {
     log('onTabMove for new child tab: move back '+aMoveInfo.toIndex+' => '+aMoveInfo.fromIndex);
     moveBack(aTab, aMoveInfo);
     return true;
+    }
   }
 }
 
