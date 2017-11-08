9e23d6082194982d82fdd927072e3282918a0854
YUKI Hiroshi <shimoda@clear-code.com>
2017-11-08 18:49:29 +0900

    Fix indent

--- webextensions/common/tree/handlers.js
+++ webextensions/common/tree/handlers.js
@@ -360,12 +360,12 @@ async function onApiTabMoved(aTabId, aMoveInfo) {
         container.alreadyMovedTabsCount--;
       }
       else {
-      container.insertBefore(movedTab, nextTab);
-      log('Tab nodes rearranged by tabs.onMoved listener:\n'+(
-        Array.slice(container.childNodes)
-          .map(aTab => aTab.id+(aTab == movedTab ? '[MOVED]' : ''))
-          .join('\n')
-          .replace(/^/gm, ' - ')));
+        container.insertBefore(movedTab, nextTab);
+        log('Tab nodes rearranged by tabs.onMoved listener:\n'+(
+          Array.slice(container.childNodes)
+            .map(aTab => aTab.id+(aTab == movedTab ? '[MOVED]' : ''))
+            .join('\n')
+            .replace(/^/gm, ' - ')));
       }
     }
     let startIndex = Math.min(aMoveInfo.fromIndex, aMoveInfo.toIndex);
