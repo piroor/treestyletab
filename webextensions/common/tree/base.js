d0589531202482a7283cf158cbf6888016679ed9
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2017-11-18 17:22:56 +0900

    More reduce number of scanned tabs

--- webextensions/common/tree/base.js
+++ webextensions/common/tree/base.js
@@ -935,13 +935,10 @@ async function sendTSTAPIMessage(aMessage, aOptions = {}) {
 }
 
 function snapshotTree(aTargetTab) {
-  var prevRoot = getRootTab(getPreviousNormalTab(aTargetTab));
-  var tabs     = [prevRoot].concat(getDescendantTabs(prevRoot));
-  var nextTab  = getNextNormalTab(aTargetTab);
-  if (tabs.indexOf(aTargetTab) < 0)
-    tabs.push(aTargetTab);
-  if (tabs.indexOf(nextTab) < 0)
-    tabs.push(nextTab);
+  var prevTab       = getPreviousNormalTab(aTargetTab);
+  var nextTab       = getNextNormalTab(aTargetTab);
+  var prevAncestors = getAncestorTabs(prevTab);
+  var tabs          = prevAncestors.reverse().concat([prevTab, aTargetTab, nextTab]);
 
   var snapshotById = {};
   function snapshotChild(aTab) {
