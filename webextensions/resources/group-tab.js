4e65d24697cb3c2a210f5264e8b593495e474912
YUKI Hiroshi <shimoda@clear-code.com>
2017-12-01 11:23:32 +0900

    Implement checkbox to switch temporary/permanent group tab

--- webextensions/resources/group-tab.js
+++ webextensions/resources/group-tab.js
@@ -5,12 +5,23 @@
 */
 'use strict';
 
+var gTemporaryCheck;
+
 document.title = getTitle();
 
 function getTitle() {
-  var title = location.search.match(/title=([^&;]*)/);
+  var title = location.search.match(/[&?]title=([^&;]*)/);
   if (!title)
     title = location.search.match(/^\?([^&;]*)/);
   return title && decodeURIComponent(title[1]) ||
            browser.i18n.getMessage('groupTab.label.default');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  gTemporaryCheck = document.querySelector('#temporary');
+  gTemporaryCheck.checked = /[&?]temporary=true/.test(location.href);
+  gTemporaryCheck.addEventListener('change', aEvent => {
+    var uri = location.href.replace(/&temporary=(true|false)|temporary=(true|false)&?/, '')
+    location.replace(`${uri}&temporary=${gTemporaryCheck.checked}`);
+  });
+}, { once: true });
