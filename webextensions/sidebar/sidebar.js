8a346472ce1a54118fb2ce5a0b5015468d05f4de
YUKI Hiroshi <shimoda@clear-code.com>
2017-12-04 12:40:58 +0900

    Fit mask to the box with mask-size

--- webextensions/sidebar/sidebar.js
+++ webextensions/sidebar/sidebar.js
@@ -355,9 +355,6 @@ function calculateDefaultSizes() {
     --scrollbar-size: ${scrollbarSize}px;
     --narrow-scrollbar-size: ${configs.narrowScrollbarSize}px;
 
-    --svg-small-icon-scale: scale(${gFaviconSize / 16}, ${gFaviconSize / 16});
-    --svg-small-icon-bg-scale: scale(${gFaviconSize / 16 * 1.3}, ${gFaviconSize / 16 * 1.3});
-
     --tab-burst-duration: ${configs.burstDuration}ms;
     --indent-duration:    ${configs.indentDuration}ms;
     --collapse-duration:  ${configs.collapseDuration}ms;
