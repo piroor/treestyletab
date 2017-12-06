126fd2615dce9c55b8cc36ee1789d356e7505f86
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2017-12-07 02:19:34 +0900

    Deactivate cache by default.
    We need more dogfooding.

--- webextensions/common/common.js
+++ webextensions/common/common.js
@@ -196,7 +196,7 @@ configs = new Configs({
   cachedExternalAddons: [],
   notifiedFeaturesVersion: 0,
 
-  useCachedTree: true,
+  useCachedTree: false,
   restoreWithPartialCache: false,
 
   requestingPermissions: null,
