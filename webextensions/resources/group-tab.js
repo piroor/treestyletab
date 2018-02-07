b61c37dd343992cd17eeee46608d9f4180009aba
YUKI Hiroshi <shimoda@clear-code.com>
2018-02-07 19:02:31 +0900

    Show pseudo tree in group tabs

--- webextensions/resources/group-tab.js
+++ webextensions/resources/group-tab.js
@@ -91,6 +91,14 @@ function init() {
     }
   });
   window.addEventListener('click', aEvent => {
+    const link = aEvent.target.closest('a');
+    if (link) {
+      browser.runtime.sendMessage({
+        type: 'treestyletab:api:focus',
+        tab:  parseInt(link.dataset.tabId)
+      });
+      return;
+    }
     if (aEvent.button == 0 &&
         !hasModifier(aEvent) &&
         aEvent.target != gTitleField) {
@@ -105,6 +113,8 @@ function init() {
       enterTitleEdit();
   });
 
+  window.addEventListener('resize', reflow);
+
   gTitle.textContent = gTitleField.value = getTitle();
 
   gTemporaryCheck = document.querySelector('#temporary');
@@ -112,6 +122,105 @@ function init() {
   gTemporaryCheck.addEventListener('change', aEvent => updateParameters());
 
   l10n.updateDocument();
+
+  updateTree();
   window.initialized = true;
 }
 //document.addEventListener('DOMContentLoaded', init, { once: true });
+
+
+async function updateTree() {
+  const tab = await browser.runtime.sendMessage({
+    type: 'treestyletab:api:get-tree',
+    tab:  'sendertab'
+  });
+  const container = document.getElementById('tabs');
+  const range = document.createRange();
+  range.selectNodeContents(container);
+  range.deleteContents();
+  range.detach();
+  const tree = document.createDocumentFragment();
+  container.appendChild(buildTabChildren(tab));
+  reflow();
+}
+
+function reflow() {
+  const container = document.getElementById('tabs');
+  columnizeTree(container.firstChild, {
+    columnWidth: '20em',
+    containerRect: container.getBoundingClientRect()
+  });
+}
+
+function buildTabItem(aTab) {
+  const item = document.createElement('li');
+
+  const link = item.appendChild(document.createElement('a'));
+  link.href = '#';
+  link.setAttribute('title', aTab.title);
+  link.dataset.tabId = aTab.id;
+
+  const icon = link.appendChild(document.createElement('img'));
+  icon.src = aTab.favIconUrl || '/sidebar/styles/icons/moon.svg';
+
+  const label = link.appendChild(document.createElement('span'));
+  label.classList.add('label');
+  label.textContent = aTab.title;
+
+  const children = buildTabChildren(aTab);
+  if (!children)
+    return item;
+
+  const fragment = document.createDocumentFragment();
+  fragment.appendChild(item);
+  const childrenWrapped = document.createElement('li');
+  childrenWrapped.appendChild(children);
+  fragment.appendChild(childrenWrapped);
+  return fragment;
+}
+
+function buildTabChildren(aTab) {
+  if (aTab.children && aTab.children.length > 0) {
+    const list = document.createElement('ul');
+    for (let child of aTab.children) {
+      list.appendChild(buildTabItem(child));
+    }
+    return list;
+  }
+  return null;
+}
+
+function columnizeTree(aTree, aOptions) {
+  aOptions = aOptions || {};
+  aOptions.columnWidth = aOptions.columnWidth || '20em';
+
+  const style = aTree.style;
+  style.columnWidth = style.MozColumnWidth = `calc(${aOptions.columnWidth})`;
+  const computedStyle = window.getComputedStyle(aTree, null);
+  aTree.columnWidth = Number((computedStyle.MozColumnWidth || computedStyle.columnWidth).replace(/px/, ''));
+  style.columnGap   = style.MozColumnGap = '1em';
+  style.columnFill  = style.MozColumnFill = 'auto';
+  style.columnCount = style.MozColumnCount = 'auto';
+
+  const containerRect = aOptions.containerRect || aTree.parentNode.getBoundingClientRect();
+  const maxWidth = containerRect.width;
+  if (aTree.columnWidth * 2 <= maxWidth ||
+      aOptions.calculateCount) {
+    style.height = style.maxHeight =
+      Math.floor(containerRect.height * 0.9) + 'px';
+
+    if (getActualColumnCount(aTree) == 1)
+      style.columnWidth = style.MozColumnWidth = '';
+  }
+  else {
+    style.height = style.maxHeight = '';
+  }
+}
+
+function getActualColumnCount(aTree) {
+  const range = document.createRange();
+  range.selectNodeContents(aTree);
+  const rect = range.getBoundingClientRect();
+  range.detach();
+  return Math.floor(rect.width / aTree.columnWidth);
+}
