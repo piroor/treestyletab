1fbca982be06a3c3c48ba5954671de5fb76bc026
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-06-11 04:56:39 +0900

    Enable no-use-before-define for ES Module
    https://eslint.org/docs/rules/no-use-before-define

--- webextensions/tools/eslint/for-module.js
+++ webextensions/tools/eslint/for-module.js
@@ -15,5 +15,10 @@ module.exports = {
     'no-undef': ['error', {
       'typeof': true,
     }],
+
+    'no-use-before-define': ['error', { // the measure for Temporary Dead Zone
+      'functions': false, //  Function declarations are hoisted.
+      'classes': true, // Class declarations are not hoisted. We should warn it.
+    }],
   },
 };
