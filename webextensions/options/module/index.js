887a5c158a246158e46d70280ef0bc25bf9fd7a9
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2018-06-12 00:27:46 +0900

    Add license header

--- webextensions/options/module/index.js
+++ webextensions/options/module/index.js
@@ -1,3 +1,8 @@
+/*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 import * as Constants from '../../common/module/constants.js';
 import * as Common from '../../common/module/common.js';
 import * as Permissions from '../../common/module/permissions.js';
