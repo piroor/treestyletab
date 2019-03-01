bcd66773b0e6e1fa6b997f5f12b0e8c368db87e3
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2019-03-02 01:07:55 +0900

    Analyze logs

--- webextensions/resources/query-logs.js
+++ webextensions/resources/query-logs.js
@@ -17,6 +17,76 @@
       log.windowId = message.windowId || 'background';
       return JSON.stringify(log);
     }).join(',\n')+',\n';
+    analyze();
   });
   browser.runtime.sendMessage({ type: 'treestyletab:request-query-logs' });
 })();
+
+function analyze() {
+  const logs = JSON.parse(`[${document.getElementById('queryLogs').textContent.replace(/,\s*$/, '')}]`);
+
+  function toString(data) {
+    return JSON.stringify(data);
+  }
+
+  function fromString(data) {
+    return JSON.parse(data);
+  }
+
+  const totalElapsedTimes = {};
+  function normalize(log) {
+    const elapsedTime = log.elapsed || log.elasped || 0;
+    log = fromString(toString(log));
+    delete log.elasped;
+    delete log.elapsed;
+    if (log.fromId) log.fromId = 'given';
+    if (log.toId) log.toId = 'given';
+    for (const key of ['id', '!id']) {
+      if (log[key])
+        log[key] = Array.isArray(log[key]) ? 'array' : (typeof log[key]);
+    }
+    const sorted = {};
+    for (const key of Object.keys(log).sort()) {
+      sorted[key] = log[key];
+    }
+    const type = toString(sorted);
+    const total = totalElapsedTimes[type] || 0;
+    totalElapsedTimes[type] = total + elapsedTime;
+    return sorted;
+  }
+  const normalizedLogs = logs.map(normalize).map(toString).sort().map(fromString);
+
+  function uniq(logs) {
+    const logTypes = logs.map(toString);
+    let lastType;
+    let lastCount;
+    const results = [];
+    for (const type of logTypes) {
+      if (type != lastType) {
+        if (lastType) {
+          results.push({
+            count: lastCount,
+            query: fromString(lastType),
+            totalElapsed: totalElapsedTimes[lastType]
+          });
+        }
+        lastType = type;
+        lastCount = 0;
+      }
+      lastCount++;
+    }
+    if (lastType)
+      results.push({
+        count: lastCount,
+        query: fromString(lastType),
+        totalElapsed: totalElapsedTimes[lastType]
+      });
+    return results;
+  }
+
+  const results = [];
+  results.push('Slowest query:\n' + toString(logs.sort((a,b) => (b.elasped || b.elapsed || 0) - (a.elasped || a.elapsed || 0))[0]));
+  results.push('Count of query tyepes:\n' + uniq(normalizedLogs).sort((a, b) => b.count - a.count).map(toString).join('\n'));
+  results.push('Sorted in total elapsed time:\n' + uniq(normalizedLogs).sort((a, b) => b.totalElapsed - a.totalElapsed).map(toString).join('\n'));
+  document.getElementById('results').textContent = '`\n' + results.join('\n') + '\n`';
+}
