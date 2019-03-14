8788aed7fd2880830a97e04eae6d2eedf7788236
YUKI Hiroshi <shimoda@clear-code.com>
2019-03-14 19:10:19 +0900

    Suppress error for already closed tabs

--- webextensions/common/Tab.js
+++ webextensions/common/Tab.js
@@ -713,7 +713,7 @@ export default class Tab {
   }
 
   async getPermanentStates() {
-    const states = await browser.sessions.getTabValue(this.tab.id, Constants.kPERSISTENT_STATES).catch(ApiTabs.createErrorHandler());
+    const states = await browser.sessions.getTabValue(this.tab.id, Constants.kPERSISTENT_STATES).catch(ApiTabs.handleMissingTabError);
     return states || [];
   }
 
