7e9243ae913b85c9b187a9d668044731fad236fa
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2019-03-20 22:08:51 +0900

    Add test for inheritance of "muted" status

--- webextensions/tests/test-tree.js
+++ webextensions/tests/test-tree.js
@@ -132,3 +132,44 @@ export async function testAutoFixupForHiddenTabs() {
   }
 }
 
+export async function testInheritMutedState() {
+  let tabs = await Utils.createTabs({
+    A: { index: 1 },
+    B: { index: 2, openerTabId: 'A' },
+    C: { index: 3, openerTabId: 'B' },
+    D: { index: 4, openerTabId: 'C' }
+  }, { windowId: win.id });
+
+  tabs = await Utils.refreshTabs(tabs);
+  {
+    const { A, B, C, D } = tabs;
+    is([
+      `${A.id}`,
+      `${A.id} => ${B.id}`,
+      `${A.id} => ${B.id} => ${C.id}`,
+      `${A.id} => ${B.id} => ${C.id} => ${D.id}`,
+    ], Utils.treeStructure(Object.values(tabs)),
+       'tabs must be initialized with specified structure');
+    is([false, false, false, false],
+       [A, B, C, D].map(tab => tab.$TST.muted),
+       'initially all tab must be unmuted');
+    is([false, false, false, false],
+       [A, B, C, D].map(tab => tab.$TST.maybeMuted),
+       'initially all tab must not inherit muted status');
+  }
+
+  await browser.tabs.update(tabs.C.id, { muted: true });
+  await wait(1000);
+
+  tabs = await Utils.refreshTabs(tabs);
+  {
+    const { A, B, C, D } = tabs;
+    is([false, false, true, false],
+       [A, B, C, D].map(tab => tab.$TST.muted),
+       'only muted tab must have muted status');
+    is([true, true, true, false],
+       [A, B, C, D].map(tab => tab.$TST.maybeMuted),
+       'ancestors must inherit "muted" state from descendant');
+  }
+}
+
