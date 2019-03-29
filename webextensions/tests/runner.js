6ca0bd21186ed736674bf8f02310f6b787db74ca
YUKI Hiroshi <shimoda@clear-code.com>
2019-03-29 13:27:15 +0900

    Reduce use of innerHTML to suppress warning on AMO

--- webextensions/tests/runner.js
+++ webextensions/tests/runner.js
@@ -169,7 +169,11 @@ function logFailure(name, error) {
     diffBlock.appendChild(document.createElement('legend')).textContent = 'Difference';
     const diff = diffBlock.appendChild(document.createElement('pre'));
     diff.classList.add('diff');
-    diff.innerHTML = Diff.readable(error.expected, error.actual, true);
+    const range = document.createRange();
+    range.selectNodeContents(diff);
+    range.collapse(false);
+    diff.appendChild(range.createContextualFragment(Diff.readable(error.expected, error.actual, true)));
+    range.detach();
   }
 }
 
