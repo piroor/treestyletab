54265f0df0f6e782fa86960952e169d76421edd9
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2019-04-24 01:29:01 +0900

    Add "reload" command for internal operations.
    It is imported from https://github.com/piroor/treestyletab/pull/2239 and https://github.com/piroor/treestyletab/tree/PowerAccess-master

--- webextensions/background/handle-misc.js
+++ webextensions/background/handle-misc.js
@@ -264,6 +264,9 @@ function onMessage(message, sender) {
 
   //log('onMessage: ', message, sender);
   switch (message.type) {
+    case Constants.kCOMMAND_RELOAD:
+     return Background.reload({ all: message.all });
+
     case Constants.kCOMMAND_REQUEST_UNIQUE_ID:
       return (async () => {
         if (!Tab.get(message.tabId))
