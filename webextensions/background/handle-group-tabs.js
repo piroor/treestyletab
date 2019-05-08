1e34dc9b84e21cb2d05015c67ab4ba721e71bf14
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2019-05-09 00:35:04 +0900

    Don't group new tabs from a pinned tab in different window #2267

--- webextensions/background/handle-group-tabs.js
+++ webextensions/background/handle-group-tabs.js
@@ -491,7 +491,9 @@ async function tryGroupNewTabsFromPinnedOpener(rootTabs) {
       return true;
     }
     const opener = Tab.getByUniqueId(tab.$TST.getAttribute(Constants.kPERSISTENT_ORIGINAL_OPENER_TAB_ID));
-    if (!opener || !opener.pinned)
+    if (!opener ||
+        !opener.pinned ||
+        opener.windowId != tab.windowId)
       return false;
     // existing and not yet grouped tab
     if (!pinnedOpeners.includes(opener))
