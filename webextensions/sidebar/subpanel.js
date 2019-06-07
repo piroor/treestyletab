f2c4b1d46ae5ffa805929379023f8deed9211c44
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2019-06-07 19:17:54 +0900

    Implement resizer

--- webextensions/sidebar/subpanel.js
+++ webextensions/sidebar/subpanel.js
@@ -18,31 +18,65 @@ export const onResized = new EventListenerManager();
 
 let mTargetWindow;
 
-const mTabBarContainer   = document.querySelector('#tabbar-container');
-const mSubPanel          = document.querySelector('#subpanel');
-const mSubPanelContainer = document.querySelector('#subpanel-container');
+const mTabBarContainer = document.querySelector('#tabbar-container');
+const mSubPanel        = document.querySelector('#subpanel');
+const mContainer       = document.querySelector('#subpanel-container');
+const mResizer         = document.querySelector('#subpanel-resizer');
 
-mSubPanelContainer.style.height = mSubPanel.style.height = 0;
+mContainer.style.height = mSubPanel.style.height = 0;
+
+const MIN_HEIGHT = 3;
+
+let mHeight = 0;
+let mDragStartY = 0;
+let mDragStartHeight = 0;
 
 export async function init() {
   mTargetWindow = TabsStore.getWindow();
+
+  mResizer.addEventListener('mousedown', event => {
+    event.stopPropagation();
+    event.preventDefault();
+    mResizer.setCapture(true);
+    mDragStartY = event.clientY;
+    mDragStartHeight = mHeight;
+    mResizer.addEventListener('mousemove', onMouseMove);
+  });
+
+  mResizer.addEventListener('mouseup', event => {
+    mResizer.removeEventListener('mousemove', onMouseMove);
+    event.stopPropagation();
+    event.preventDefault();
+    document.releaseCapture();
+    mHeight = Math.max(MIN_HEIGHT, mDragStartHeight - (event.clientY - mDragStartY));
+    browser.sessions.setWindowValue(mTargetWindow, Constants.kWINDOW_STATE_SUBPANEL_HEIGHT, mHeight).catch(ApiTabs.createErrorHandler());
+    applyHeight();
+  });
 
   const [url, height] = await Promise.all([
     browser.sessions.getWindowValue(mTargetWindow, Constants.kWINDOW_STATE_SUBPANEL_URL).catch(ApiTabs.createErrorHandler()),
     browser.sessions.getWindowValue(mTargetWindow, Constants.kWINDOW_STATE_SUBPANEL_HEIGHT).catch(ApiTabs.createErrorHandler())
   ]);
+  mHeight = height || 1;
 
   load({ url });
-  setHeight(height);
+  applyHeight();
 }
 
 export function load(params = {}) {
   mSubPanel.setAttribute('src', params.url || 'about:blank');
 }
 
-export function setHeight(height) {
-  height = 300; // height || 0;
-  mSubPanelContainer.style.height = mSubPanel.style.height = `${height}px`;
-  mTabBarContainer.style.bottom = `${height}px`;
+function applyHeight() {
+  mContainer.style.height = `${mHeight}px`;
+  mSubPanel.style.height = `calc(${mHeight}px - var(--resizer-size))`;
+  mTabBarContainer.style.bottom = `${mHeight}px`;
   onResized.dispatch();
+}
+
+function onMouseMove(event) {
+  event.stopPropagation();
+  event.preventDefault();
+  mHeight = Math.max(MIN_HEIGHT, mDragStartHeight - (event.clientY - mDragStartY));
+  applyHeight();
 }
