4a54ba7cfaa252761b57374dc2cb64b3d77a577c
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2019-10-21 18:25:17 +0900

    Remove needless import

--- webextensions/sidebar/components/TabLabelElement.js
+++ webextensions/sidebar/components/TabLabelElement.js
@@ -4,8 +4,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-import * as Constants from '/common/constants.js';
-
 export const kTAB_LABEL_ELEMENT_NAME = 'tab-label';
 
 const KLABEL_CLASS_NAME   = 'label';
