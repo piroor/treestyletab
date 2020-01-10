c679e769f05158353752fb29339c79c362848a30
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-01-10 10:16:02 +0900

    Fix indent

--- webextensions/options/init.js
+++ webextensions/options/init.js
@@ -204,214 +204,214 @@ window.addEventListener('DOMContentLoaded', async () => {
 
   await configs.$loaded;
 
-    const focusedItem = document.querySelector(':target');
-    for (const fieldset of document.querySelectorAll('fieldset.collapsible')) {
-      if (configs.optionsExpandedGroups.includes(fieldset.id) ||
-          (focusedItem && fieldset.contains(focusedItem)))
-        fieldset.classList.remove('collapsed');
+  const focusedItem = document.querySelector(':target');
+  for (const fieldset of document.querySelectorAll('fieldset.collapsible')) {
+    if (configs.optionsExpandedGroups.includes(fieldset.id) ||
+        (focusedItem && fieldset.contains(focusedItem)))
+      fieldset.classList.remove('collapsed');
+    else
+      fieldset.classList.add('collapsed');
+
+    const onChangeCollapsed = () => {
+      if (!fieldset.id)
+        return;
+      const otherExpandedSections = configs.optionsExpandedGroups.filter(id => id != fieldset.id);
+      if (fieldset.classList.contains('collapsed'))
+        configs.optionsExpandedGroups = otherExpandedSections;
       else
-        fieldset.classList.add('collapsed');
+        configs.optionsExpandedGroups = otherExpandedSections.concat([fieldset.id]);
+    };
 
-      const onChangeCollapsed = () => {
-        if (!fieldset.id)
-          return;
-        const otherExpandedSections = configs.optionsExpandedGroups.filter(id => id != fieldset.id);
-        if (fieldset.classList.contains('collapsed'))
-          configs.optionsExpandedGroups = otherExpandedSections;
-        else
-          configs.optionsExpandedGroups = otherExpandedSections.concat([fieldset.id]);
-      };
+    const legend = fieldset.querySelector(':scope > legend');
+    legend.addEventListener('click', () => {
+      fieldset.classList.toggle('collapsed');
+      onChangeCollapsed();
+    });
+    legend.addEventListener('keydown', event => {
+      if (event.key != 'Enter')
+        return;
+      fieldset.classList.toggle('collapsed');
+      onChangeCollapsed();
+    });
+  }
 
-      const legend = fieldset.querySelector(':scope > legend');
-      legend.addEventListener('click', () => {
-        fieldset.classList.toggle('collapsed');
-        onChangeCollapsed();
-      });
-      legend.addEventListener('keydown', event => {
-        if (event.key != 'Enter')
-          return;
-        fieldset.classList.toggle('collapsed');
-        onChangeCollapsed();
-      });
-    }
-
-    for (const heading of document.querySelectorAll('body > section > h1')) {
-      const section = heading.parentNode;
-      section.style.maxHeight = `${heading.offsetHeight}px`;
-      if (!configs.optionsExpandedSections.includes(section.id) &&
+  for (const heading of document.querySelectorAll('body > section > h1')) {
+    const section = heading.parentNode;
+    section.style.maxHeight = `${heading.offsetHeight}px`;
+    if (!configs.optionsExpandedSections.includes(section.id) &&
           (!focusedItem || !section.contains(focusedItem)))
-        section.classList.add('collapsed');
-      heading.addEventListener('click', () => {
-        section.classList.toggle('collapsed');
-        const otherExpandedSections = configs.optionsExpandedSections.filter(id => id != section.id);
-        if (section.classList.contains('collapsed'))
-          configs.optionsExpandedSections = otherExpandedSections;
-        else
-          configs.optionsExpandedSections = otherExpandedSections.concat([section.id]);
-      });
-    }
+      section.classList.add('collapsed');
+    heading.addEventListener('click', () => {
+      section.classList.toggle('collapsed');
+      const otherExpandedSections = configs.optionsExpandedSections.filter(id => id != section.id);
+      if (section.classList.contains('collapsed'))
+        configs.optionsExpandedSections = otherExpandedSections;
+      else
+        configs.optionsExpandedSections = otherExpandedSections.concat([section.id]);
+    });
+  }
 
-    Permissions.isGranted(Permissions.BOOKMARKS).then(granted => updateBookmarksUI(granted));
+  Permissions.isGranted(Permissions.BOOKMARKS).then(granted => updateBookmarksUI(granted));
 
-    Permissions.bindToCheckbox(
-      Permissions.ALL_URLS,
-      document.querySelector('#allUrlsPermissionGranted'),
-      { onChanged: (granted) => configs.skipCollapsedTabsForTabSwitchingShortcuts = granted }
-    );
-    Permissions.bindToCheckbox(
-      Permissions.BOOKMARKS,
-      document.querySelector('#bookmarksPermissionGranted'),
-      { onChanged: (granted) => updateBookmarksUI(granted) }
-    );
-    Permissions.bindToCheckbox(
-      Permissions.TAB_HIDE,
-      document.querySelector('#tabHidePermissionGranted'),
-      { onChanged: async (granted) => {
-        if (granted) {
-          // try to hide/show the tab to ensure the permission is really granted
-          const activeTabs = await browser.tabs.query({ active: true, currentWindow: true });
-          const tab = await browser.tabs.create({ active: false, windowId: activeTabs[0].windowId });
-          await wait(200);
-          let aborted = false;
-          const onRemoved = tabId => {
-            if (tabId != tab.id)
-              return;
-            aborted = true;
-            browser.tabs.onRemoved.removeListener(onRemoved);
-            // eslint-disable-next-line no-use-before-define
-            browser.tabs.onUpdated.removeListener(onUpdated);
-          };
-          const onUpdated = async (tabId, changeInfo, tab) => {
-            if (tabId != tab.id ||
+  Permissions.bindToCheckbox(
+    Permissions.ALL_URLS,
+    document.querySelector('#allUrlsPermissionGranted'),
+    { onChanged: (granted) => configs.skipCollapsedTabsForTabSwitchingShortcuts = granted }
+  );
+  Permissions.bindToCheckbox(
+    Permissions.BOOKMARKS,
+    document.querySelector('#bookmarksPermissionGranted'),
+    { onChanged: (granted) => updateBookmarksUI(granted) }
+  );
+  Permissions.bindToCheckbox(
+    Permissions.TAB_HIDE,
+    document.querySelector('#tabHidePermissionGranted'),
+    { onChanged: async (granted) => {
+      if (granted) {
+        // try to hide/show the tab to ensure the permission is really granted
+        const activeTabs = await browser.tabs.query({ active: true, currentWindow: true });
+        const tab = await browser.tabs.create({ active: false, windowId: activeTabs[0].windowId });
+        await wait(200);
+        let aborted = false;
+        const onRemoved = tabId => {
+          if (tabId != tab.id)
+            return;
+          aborted = true;
+          browser.tabs.onRemoved.removeListener(onRemoved);
+          // eslint-disable-next-line no-use-before-define
+          browser.tabs.onUpdated.removeListener(onUpdated);
+        };
+        const onUpdated = async (tabId, changeInfo, tab) => {
+          if (tabId != tab.id ||
                 !('hidden' in changeInfo))
-              return;
-            await wait(60 * 1000);
-            if (aborted)
-              return;
-            await browser.tabs.show([tab.id]);
-            await browser.tabs.remove(tab.id);
-          };
-          browser.tabs.onRemoved.addListener(onRemoved);
-          browser.tabs.onUpdated.addListener(onUpdated);
-          await browser.tabs.hide([tab.id]);
-        }
-      }}
-    );
+            return;
+          await wait(60 * 1000);
+          if (aborted)
+            return;
+          await browser.tabs.show([tab.id]);
+          await browser.tabs.remove(tab.id);
+        };
+        browser.tabs.onRemoved.addListener(onRemoved);
+        browser.tabs.onUpdated.addListener(onUpdated);
+        await browser.tabs.hide([tab.id]);
+      }
+    }}
+  );
 
 
-    for (const checkbox of document.querySelectorAll('p input[type="checkbox"][id^="logFor-"]')) {
-      checkbox.addEventListener('change', onChangeSlaveChacekbox);
-      checkbox.checked = configs.logFor[checkbox.id.replace(/^logFor-/, '')];
-    }
-    for (const checkbox of document.querySelectorAll('legend input[type="checkbox"][id^="logFor-"]')) {
-      checkbox.checked = isAllSlavesChecked(checkbox);
-      checkbox.addEventListener('change', onChangeMasterChacekbox);
-    }
+  for (const checkbox of document.querySelectorAll('p input[type="checkbox"][id^="logFor-"]')) {
+    checkbox.addEventListener('change', onChangeSlaveChacekbox);
+    checkbox.checked = configs.logFor[checkbox.id.replace(/^logFor-/, '')];
+  }
+  for (const checkbox of document.querySelectorAll('legend input[type="checkbox"][id^="logFor-"]')) {
+    checkbox.checked = isAllSlavesChecked(checkbox);
+    checkbox.addEventListener('change', onChangeMasterChacekbox);
+  }
 
-    for (const previewImage of document.querySelectorAll('select ~ .preview-image')) {
-      const container = previewImage.parentNode;
-      container.classList.add('has-preview-image');
-      const select = container.querySelector('select');
-      container.dataset.value = select.dataset.value = select.value;
-      container.addEventListener('mouseover', event => {
-        if (event.target != select &&
+  for (const previewImage of document.querySelectorAll('select ~ .preview-image')) {
+    const container = previewImage.parentNode;
+    container.classList.add('has-preview-image');
+    const select = container.querySelector('select');
+    container.dataset.value = select.dataset.value = select.value;
+    container.addEventListener('mouseover', event => {
+      if (event.target != select &&
             select.contains(event.target))
-          return;
-        const rect = select.getBoundingClientRect();
-        previewImage.style.left = `${rect.left}px`;
-        previewImage.style.top  = `${rect.top - 5 - previewImage.offsetHeight}px`;
-      });
-      select.addEventListener('change', () => {
-        container.dataset.value = select.dataset.value = select.value;
-      });
-      select.addEventListener('mouseover', event => {
-        if (event.target == select)
-          return;
-        container.dataset.value = select.dataset.value = event.target.value;
-      });
-      select.addEventListener('mouseout', () => {
-        container.dataset.value = select.dataset.value = select.value;
-      });
-    }
+        return;
+      const rect = select.getBoundingClientRect();
+      previewImage.style.left = `${rect.left}px`;
+      previewImage.style.top  = `${rect.top - 5 - previewImage.offsetHeight}px`;
+    });
+    select.addEventListener('change', () => {
+      container.dataset.value = select.dataset.value = select.value;
+    });
+    select.addEventListener('mouseover', event => {
+      if (event.target == select)
+        return;
+      container.dataset.value = select.dataset.value = event.target.value;
+    });
+    select.addEventListener('mouseout', () => {
+      container.dataset.value = select.dataset.value = select.value;
+    });
+  }
 
-    browser.runtime.sendMessage({
-      type: TSTAPI.kCOMMAND_GET_ADDONS
-    }).then(addons => {
-      const description = document.getElementById('externalAddonPermissionsGroupDescription');
-      const range = document.createRange();
-      range.selectNodeContents(description);
-      description.appendChild(range.createContextualFragment(browser.i18n.getMessage('config_externaladdonpermissions_description')));
-      range.detach();
+  browser.runtime.sendMessage({
+    type: TSTAPI.kCOMMAND_GET_ADDONS
+  }).then(addons => {
+    const description = document.getElementById('externalAddonPermissionsGroupDescription');
+    const range = document.createRange();
+    range.selectNodeContents(description);
+    description.appendChild(range.createContextualFragment(browser.i18n.getMessage('config_externaladdonpermissions_description')));
+    range.detach();
 
-      const container = document.getElementById('externalAddonPermissions');
-      for (const addon of addons) {
-        if (addon.id == browser.runtime.id)
-          continue;
-        const row = document.createElement('tr');
+    const container = document.getElementById('externalAddonPermissions');
+    for (const addon of addons) {
+      if (addon.id == browser.runtime.id)
+        continue;
+      const row = document.createElement('tr');
 
-        const nameCell = row.appendChild(document.createElement('td'));
-        const nameLabel = nameCell.appendChild(document.createElement('label'));
-        nameLabel.appendChild(document.createTextNode(addon.label));
-        const controlledId = `api-permissions-${encodeURIComponent(addon.id)}`;
-        nameLabel.setAttribute('for', controlledId);
+      const nameCell = row.appendChild(document.createElement('td'));
+      const nameLabel = nameCell.appendChild(document.createElement('label'));
+      nameLabel.appendChild(document.createTextNode(addon.label));
+      const controlledId = `api-permissions-${encodeURIComponent(addon.id)}`;
+      nameLabel.setAttribute('for', controlledId);
 
-        const incognitoCell = row.appendChild(document.createElement('td'));
-        const incognitoLabel = incognitoCell.appendChild(document.createElement('label'));
-        const incognitoCheckbox = incognitoLabel.appendChild(document.createElement('input'));
-        if (addon.permissions.length == 0)
-          incognitoCheckbox.setAttribute('id', controlledId);
-        incognitoCheckbox.setAttribute('type', 'checkbox');
-        incognitoCheckbox.checked = configs.incognitoAllowedExternalAddons.includes(addon.id);
-        incognitoCheckbox.addEventListener('change', () => {
-          const updatedValue = new Set(configs.incognitoAllowedExternalAddons);
-          if (incognitoCheckbox.checked)
-            updatedValue.add(addon.id);
-          else
-            updatedValue.delete(addon.id);
-          configs.incognitoAllowedExternalAddons = Array.from(updatedValue);
+      const incognitoCell = row.appendChild(document.createElement('td'));
+      const incognitoLabel = incognitoCell.appendChild(document.createElement('label'));
+      const incognitoCheckbox = incognitoLabel.appendChild(document.createElement('input'));
+      if (addon.permissions.length == 0)
+        incognitoCheckbox.setAttribute('id', controlledId);
+      incognitoCheckbox.setAttribute('type', 'checkbox');
+      incognitoCheckbox.checked = configs.incognitoAllowedExternalAddons.includes(addon.id);
+      incognitoCheckbox.addEventListener('change', () => {
+        const updatedValue = new Set(configs.incognitoAllowedExternalAddons);
+        if (incognitoCheckbox.checked)
+          updatedValue.add(addon.id);
+        else
+          updatedValue.delete(addon.id);
+        configs.incognitoAllowedExternalAddons = Array.from(updatedValue);
+        browser.runtime.sendMessage({
+          type: TSTAPI.kCOMMAND_NOTIFY_PERMISSION_CHANGED,
+          id:   addon.id
+        });
+      });
+
+      const permissionsCell = row.appendChild(document.createElement('td'));
+      if (addon.permissions.length > 0) {
+        const permissionsLabel = permissionsCell.appendChild(document.createElement('label'));
+        const permissionsCheckbox = permissionsLabel.appendChild(document.createElement('input'));
+        permissionsCheckbox.setAttribute('id', controlledId);
+        permissionsCheckbox.setAttribute('type', 'checkbox');
+        permissionsCheckbox.checked = addon.permissionsGranted;
+        permissionsCheckbox.addEventListener('change', () => {
           browser.runtime.sendMessage({
-            type: TSTAPI.kCOMMAND_NOTIFY_PERMISSION_CHANGED,
-            id:   addon.id
+            type:        TSTAPI.kCOMMAND_SET_API_PERMISSION,
+            id:          addon.id,
+            permissions: permissionsCheckbox.checked ? addon.permissions : addon.permissions.map(permission => `!  ${permission}`)
           });
         });
-
-        const permissionsCell = row.appendChild(document.createElement('td'));
-        if (addon.permissions.length > 0) {
-          const permissionsLabel = permissionsCell.appendChild(document.createElement('label'));
-          const permissionsCheckbox = permissionsLabel.appendChild(document.createElement('input'));
-          permissionsCheckbox.setAttribute('id', controlledId);
-          permissionsCheckbox.setAttribute('type', 'checkbox');
-          permissionsCheckbox.checked = addon.permissionsGranted;
-          permissionsCheckbox.addEventListener('change', () => {
-            browser.runtime.sendMessage({
-              type:        TSTAPI.kCOMMAND_SET_API_PERMISSION,
-              id:          addon.id,
-              permissions: permissionsCheckbox.checked ? addon.permissions : addon.permissions.map(permission => `!  ${permission}`)
-            });
-          });
-          const permissionNames = addon.permissions.map(permission => {
-            try {
-              return browser.i18n.getMessage(`api_requestedPermissions_type_${permission}`) || permission;
-            }
-            catch(_error) {
-              return permission;
-            }
-          }).join(', ');
-          permissionsLabel.appendChild(document.createTextNode(permissionNames));
-        }
-
-        container.appendChild(row);
+        const permissionNames = addon.permissions.map(permission => {
+          try {
+            return browser.i18n.getMessage(`api_requestedPermissions_type_${permission}`) || permission;
+          }
+          catch(_error) {
+            return permission;
+          }
+        }).join(', ');
+        permissionsLabel.appendChild(document.createTextNode(permissionNames));
       }
-    });
 
-    options.buildUIForAllConfigs(document.querySelector('#group-allConfigs'));
-    onConfigChanged('successorTabControlLevel');
-    onConfigChanged('showExpertOptions');
-    await wait(0);
-    onConfigChanged('closeParentBehaviorMode');
+      container.appendChild(row);
+    }
+  });
 
-    if (focusedItem)
-      focusedItem.scrollIntoView();
+  options.buildUIForAllConfigs(document.querySelector('#group-allConfigs'));
+  onConfigChanged('successorTabControlLevel');
+  onConfigChanged('showExpertOptions');
+  await wait(0);
+  onConfigChanged('closeParentBehaviorMode');
 
-    document.documentElement.classList.add('initialized');
+  if (focusedItem)
+    focusedItem.scrollIntoView();
+
+  document.documentElement.classList.add('initialized');
 }, { once: true });
