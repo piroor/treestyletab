955cf0ffe97202a58076821cce24214153a1f527
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-03-05 23:45:58 +0900

    Relicense more DOM Updater related codes under MIT License

--- webextensions/tests/assert.js
+++ webextensions/tests/assert.js
@@ -1,7 +1,7 @@
 /*
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ license: The MIT License, Copyright (c) 2020 YUKI "Piro" Hiroshi
+ original:
+   https://github.com/piroor/treestyletab/blob/master/webextensions/tests/assert.js
 */
 'use strict';
 
