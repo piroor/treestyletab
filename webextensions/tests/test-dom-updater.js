86a697e3afb0045a0d53b5cbb7cd06f6cfa050cb
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-03-09 14:17:59 +0900

    More heavy benchmark

--- webextensions/tests/test-dom-updater.js
+++ webextensions/tests/test-dom-updater.js
@@ -57,55 +57,57 @@ function assertUpdatedWithMorphdomDocumentFragment(from, to) {
   return end - start;
 }
 
-function benchamrk(from, to) {
+async function benchamrk(from, to) {
   const range = document.createRange();
-  const tries = 500;
+  const tries = 5000;
+  let totalDOMUpdater = 0;
+  let totalMorphdomString = 0;
+  let totalMorphdomDocumentFragment = 0;
   const tasks = [
     (() => {
       range.selectNodeContents(container);
       range.deleteContents();
-      let total = 0;
-      for (let i = 0; i < tries; i++) {
-        total += assertUpdatedWithDOMUpdater(from, to);
-      }
-      result.appendChild(document.createElement('li')).textContent = `DOMUpdater: DocumentFragment ${total}`;
+      totalDOMUpdater += assertUpdatedWithDOMUpdater(from, to);
     }),
     (() => {
       range.selectNodeContents(container);
       range.deleteContents();
-      let total = 0;
-      for (let i = 0; i < tries; i++) {
-        total += assertUpdatedWithMorphdomString(from, to);
-      }
-      result.appendChild(document.createElement('li')).textContent = `morphdom: string ${total}`;
+      totalMorphdomString += assertUpdatedWithMorphdomString(from, to);
     }),
     (() => {
       range.selectNodeContents(container);
       range.deleteContents();
-      let total = 0;
-      for (let i = 0; i < tries; i++) {
-        total += assertUpdatedWithMorphdomDocumentFragment(from, to);
-      }
-      result.appendChild(document.createElement('li')).textContent = `morphdom: DocumentFragment ${total}`;
+      totalMorphdomDocumentFragment += assertUpdatedWithMorphdomDocumentFragment(from, to);
     })
   ];
-  // shuffle with Fisher–Yates
-  for (let i = tasks.length - 1; i > 0; i--) {
-    const randomIndex = Math.floor(Math.random() * (i + 1));
-    const element = tasks[i];
-    tasks[i] = tasks[randomIndex];
-    tasks[randomIndex] = element;
-  }
-  for (const task of tasks) {
-    task();
+  let start = 0;
+  for (let i = 0; i < tries; i++) {
+    // shuffle with Fisher–Yates
+    for (let i = tasks.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      const element = tasks[i];
+      tasks[i] = tasks[randomIndex];
+      tasks[randomIndex] = element;
+    }
+    for (const task of tasks) {
+      task();
+    }
+    if (Date.now() - start > 3000) {
+      console.log('3sec past, wait for next tick');
+      await new Promise(resolve => setTimeout(resolve, 0));
+      start = Date.now();
+    }
   }
   range.selectNodeContents(container);
   range.deleteContents();
   range.detach();
+  result.appendChild(document.createElement('li')).textContent = `DOMUpdater: DocumentFragment ${totalDOMUpdater}`;
+  result.appendChild(document.createElement('li')).textContent = `morphdom: string ${totalMorphdomString}`;
+  result.appendChild(document.createElement('li')).textContent = `morphdom: DocumentFragment ${totalMorphdomDocumentFragment}`;
 }
 
-export function testBenchmark() {
-  benchamrk(
+export async function testBenchmark() {
+  await benchamrk(
     `
       <span id="item1">contents
         <span id="item1-1">contents</span>
