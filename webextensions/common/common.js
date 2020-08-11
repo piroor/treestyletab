94948e1a84218ab16958841f798cf655c73b155c
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-08-11 14:57:18 +0900

    Use 8 slots fully

--- webextensions/common/common.js
+++ webextensions/common/common.js
@@ -417,7 +417,7 @@ export function loadUserStyleRules() {
 }
 
 export function saveUserStyleRules(style) {
-  [...chunkString(style, Constants.kSYNC_STORAGE_SAFE_QUOTA), ...new Array(6)].slice(0, 6).forEach((chunk, index) => {
+  [...chunkString(style, Constants.kSYNC_STORAGE_SAFE_QUOTA), ...new Array(8)].slice(0, 8).forEach((chunk, index) => {
     const key = `userStyleRules${index}`;
     if (key in configs)
       configs[key] = chunk || '';
