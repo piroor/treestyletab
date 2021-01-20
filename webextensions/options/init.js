e71c0166ca18bb8913b2f46467b88aa0cce09cd1
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2021-01-20 13:00:07 +0900

    Include all CodeMirror themes #2704

--- webextensions/options/init.js
+++ webextensions/options/init.js
@@ -39,6 +39,82 @@ if ((location.hash && location.hash != '#') ||
     /independent=true/.test(location.search))
   document.body.classList.add('independent');
 
+const CODEMIRROR_THEMES = `
+3024-day
+3024-night
+abcdef
+ambiance-mobile
+ambiance
+ayu-dark
+ayu-mirage
+base16-dark
+base16-light
+bespin
+blackboard
+cobalt
+colorforth
+darcula
+dracula
+duotone-dark
+duotone-light
+eclipse
+elegant
+erlang-dark
+gruvbox-dark
+hopscotch
+icecoder
+idea
+isotope
+lesser-dark
+liquibyte
+lucario
+material-darker
+material-ocean
+material-palenight
+material
+mbo
+mdn-like
+midnight
+monokai
+moxer
+neat
+neo
+night
+nord
+oceanic-next
+panda-syntax
+paraiso-dark
+paraiso-light
+pastel-on-dark
+railscasts
+rubyblue
+seti
+shadowfox
+solarized
+ssms
+the-matrix
+tomorrow-night-bright
+tomorrow-night-eighties
+ttcn
+twilight
+vibrant-ink
+xq-dark
+xq-light
+yeti
+yonce
+zenburn
+`.trim().split(/\s+/);
+{
+  const range = document.createRange();
+  range.selectNodeContents(document.querySelector('head'));
+  range.collapse(false);
+  range.insertNode(range.createContextualFragment(CODEMIRROR_THEMES.map(theme =>
+    `<link rel="stylesheet"
+           type="text/css"
+           href="/extlib/codemirror-theme/${theme}.css"/>`)));
+  range.detach();
+}
+
 const mUserStyleRulesField = document.getElementById('userStyleRulesField');
 let mUserStyleRulesFieldEditor;
 
