28126cc6afc4b39c4548dd292daafd34efa8fd28
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2021-04-21 23:36:00 +0900

    Bump cache version to make cache obsolete

--- webextensions/sidebar/sidebar-cache.js
+++ webextensions/sidebar/sidebar-cache.js
@@ -40,7 +40,7 @@ function log(...args) {
   internalLogger('sidebar/sidebar-cache', ...args);
 }
 
-const kCONTENTS_VERSION = 24;
+const kCONTENTS_VERSION = 25;
 
 export const onRestored = new EventListenerManager();
 
