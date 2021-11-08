24092d3864cd5eca76100f9b45981309db01705c
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2021-11-08 23:02:28 +0900

    Uniform coding style

--- webextensions/sidebar/components/TabLabelElement.js
+++ webextensions/sidebar/components/TabLabelElement.js
@@ -12,9 +12,9 @@ const kCONTENT_CLASS_NAME = `${KLABEL_CLASS_NAME}-content`;
 const kATTR_NAME_VALUE = 'value';
 
 // https://stackoverflow.com/questions/12006095/javascript-how-to-check-if-character-is-rtl
-const LTR_CHARS = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF'+'\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF';
+const LTR_CHARS = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF';
 const RTL_CHARS = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC';
-const RTL_DIRECTION_CHECK = new RegExp('^[^'+LTR_CHARS+']*['+RTL_CHARS+']');
+const RTL_DIRECTION_CHECK = new RegExp(`^[^${LTR_CHARS}]*[${RTL_CHARS}]`);
 function isRTL(string) {
   return RTL_DIRECTION_CHECK.test(string);
 }
