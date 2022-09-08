f90c9cd854385508b3065102e6f1b7d852122baa
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2022-09-08 22:24:34 +0900

    Unify coding style

--- webextensions/common/splic-css-selector.js
+++ webextensions/common/splic-css-selector.js
@@ -5,38 +5,40 @@
 
 'use strict';
 
-/* eslint-disable no-undef,no-var */
-module.exports = function splitSelectors(selectors) {
-	if (isAtRule(selectors)) {
-		return [selectors];
-	}
-	var splitted = [];
-	var parens = 0;
-	var angulars = 0;
-	var soFar = '';
-	for (var i = 0, len = selectors.length; i < len; i++) {
-		var char = selectors[i];
-		if (char === '(') {
-			parens += 1;
-		} else if (char === ')') {
-			parens -= 1;
-		} else if (char === '[') {
-			angulars += 1;
-		} else if (char === ']') {
-			angulars -= 1;
-		} else if (char === ',') {
-			if (!parens && !angulars) {
-				splitted.push(soFar.trim());
-				soFar = '';
-				continue;
-			}
-		}
-		soFar += char;
-	}
-	splitted.push(soFar.trim());
-	return splitted;
-};
+export function split(selectors) {
+  if (isAtRule(selectors)) {
+    return [selectors];
+  }
+  const splitted = [];
+  let parens     = 0;
+  let angulars   = 0;
+  let soFar      = '';
+  for (const char of selectors) {
+    if (char === '(') {
+      parens++;
+    }
+    else if (char === ')') {
+      parens--;
+    }
+    else if (char === '[') {
+      angulars++;
+    }
+    else if (char === ']') {
+      angulars--;
+    }
+    else if (char === ',') {
+      if (!parens && !angulars) {
+        splitted.push(soFar.trim());
+        soFar = '';
+        continue;
+      }
+    }
+    soFar += char;
+  }
+  splitted.push(soFar.trim());
+  return splitted;
+}
 
 function isAtRule(selector) {
-	return selector.indexOf('@') === 0;
+  return selector.startsWith('@');
 }
