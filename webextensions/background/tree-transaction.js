432e2d33e6c816183b18b00e3a323cab037b4e15
TOKUNAGA Hiroyuki <96674+tkng@users.noreply.github.com>
2026-03-08 00:10:49 +0900

    docs(tree-transaction): add MPL license header

--- webextensions/background/tree-transaction.js
+++ webextensions/background/tree-transaction.js
@@ -1,3 +1,27 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is the Tree Style Tab.
+ *
+ * The Initial Developer of the Original Code is YUKI "Piro" Hiroshi.
+ * Portions created by the Initial Developer are Copyright (C) 2011-2026
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s): YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
+ *                 tkng (https://github.com/tkng)
+ *
+ * ***** END LICENSE BLOCK ******/
+
 // TreeTransaction: Manages tree structure changes and batches sidebar messages.
 //
 // High-level operations:
