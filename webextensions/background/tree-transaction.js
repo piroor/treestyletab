5455d2b93b7303d42190f04a526fc1973d247cd5
TOKUNAGA Hiroyuki <96674+tkng@users.noreply.github.com>
2026-03-09 03:15:06 +0900

    chore(tree-transaction): update license to MPL 2.0

--- webextensions/background/tree-transaction.js
+++ webextensions/background/tree-transaction.js
@@ -1,15 +1,7 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
  * The Original Code is the Tree Style Tab.
  *
@@ -19,8 +11,7 @@
  *
  * Contributor(s): YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
  *                 tkng (https://github.com/tkng)
- *
- * ***** END LICENSE BLOCK ******/
+ */
 
 // TreeTransaction: Manages tree structure changes and batches sidebar messages.
 //
