2a0fbb0cc09e3789fb0e2c2586fdc8c326fc5193
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2026-09-15 01:27:09 +0900

    Don't treat our custom pages injectable

--- webextensions/common/permissions.js
+++ webextensions/common/permissions.js
@@ -54,7 +54,7 @@ for (const permissions of [ALL_URLS, BOOKMARKS, CLIPBOARD_READ, BROWSER_SETTINGS
 }
 
 
-const CUSTOM_PANEL_AVAILABLE_URLS_MATCHER = new RegExp(`^((https?|data):|moz-extension://${window.location.host}/)`);
+const CUSTOM_PANEL_AVAILABLE_URLS_MATCHER = /^(about:blank|(https?|file):\/\/)/;
 
 export async function canInjectScriptToTab(tab) {
   if (!tab ||
